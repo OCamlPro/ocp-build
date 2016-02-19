7579f2b6cebca5f994a37b0e2e3c6c9069b8e27b
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2016-02-19 16:05:50 +0100

    Fix push-opam

--- push-opam.sh
+++ push-opam.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
 OPAMREPO=$HOME/WindowsSharedFolder/opam-repository-with-archives
-(cd $OPAMREPO && git checkout master && git pull ocaml master)
-
 . ./autoconf/Makefile.config
 echo VERSION=$VERSION
 
-mkdir $OPAMREPO/package/ocp-build/ocp-build.$VERSION
-cp opam descr findlib $OPAMREPO/package/ocp-build/ocp-build.$VERSION/
+(cd $OPAMREPO && git checkout master && git pull ocaml master)
 
-URL=http://github.com/OCamlPro/ocp-build/archive/${VERSION}.tar.gz
-wget ${URL}
-MD5SUM=$(md5sum ${VERSION}.tar.gz)
-echo 'archive: "'${URL}'"' > $OPAMREPO/package/ocp-build/ocp-build.$VERSION/url
-echo 'checksum: "'${MD5SUM}'"' >> $OPAMREPO/package/ocp-build/ocp-build.$VERSION/url
+
+if [ -e ${OPAMREPO}/packages/${PACKAGE}/${PACKAGE}.${VERSION} ]; then
+    echo "Error: directory for ${VERSION} already exists"
+    echo ${OPAMREPO}/packages/${PACKAGE}/${PACKAGE}.${VERSION}
+    exit 2
+fi
+
+mkdir $OPAMREPO/packages/${PACKAGE}/${PACKAGE}.$VERSION || exit 2
+cp opam descr findlib $OPAMREPO/packages/${PACKAGE}/${PACKAGE}.$VERSION/ || exit 2
+
+URL=http://github.com/OCamlPro/${PACKAGE}/archive/${VERSION}.tar.gz
+rm -f ${VERSION}.tar.gz
+wget ${URL} || exit 2
+MD5SUM=$(md5sum ${VERSION}.tar.gz | cut -b 1-32 ) 
+echo 'archive: "'${URL}'"' > $OPAMREPO/packages/${PACKAGE}/${PACKAGE}.$VERSION/url
+echo 'checksum: "'${MD5SUM}'"' >> $OPAMREPO/packages/${PACKAGE}/${PACKAGE}.$VERSION/url
 
 echo Generated url file:
-cat $OPAMREPO/package/ocp-build/ocp-build.$VERSION/url
+cat $OPAMREPO/packages/${PACKAGE}/${PACKAGE}.$VERSION/url
 
 (cd ${OPAMREPO} &&
-        git checkout -b ocp-build.${VERSION} &&
-        git add package/ocp-build/ocp-build.${VERSION} &&
-        git commit -m "Add ocp-build.${VERSION}"
- package/ocp-build/ocp-build.${VERSION} &&
-     git push)
+        git checkout -b ${PACKAGE}.${VERSION} &&
+        git add packages/${PACKAGE}/${PACKAGE}.${VERSION} &&
+        git commit -m "Add ${PACKAGE}.${VERSION}" packages/${PACKAGE}/${PACKAGE}.${VERSION} &&
+        git push origin ${PACKAGE}.${VERSION})
