69f35be8fb109b1637769c1d7fd11e6d984a91e0
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2016-05-25 16:40:22 +0200

    dkjd

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -13,11 +13,9 @@ opam --git-version
 
 PREFIX=$HOME/.opam/$OCAML_VERSION
 
-./configure --prefix ${PREFIX} --with-metadir=${PREFIX}/lib
-make
-make install
+opam pin add -y ocp-build .
 
-  echo '${TRAVIS_DIR} = ' ${TRAVIS_DIR}
+  echo '${TRAVIS_BUILD_DIR} = ' ${TRAVIS_BUILD_DIR}
   echo '${TRAVIS_PULL_REQUEST} = ' ${TRAVIS_PULL_REQUEST}
   echo '${TRAVIS_REPO_SLUG} = ' ${TRAVIS_REPO_SLUG}
   echo '${TRAVIS_TAG} = ' ${TRAVIS_TAG}
@@ -29,9 +27,13 @@ if [ "${OCAML_VERSION}" != "4.02.3" ] ; then
    echo No lint
 else
 
-    ocaml ./travis2github.ml
-    
-   curl --user "ocp-lint-bot:${GITHUB_TOKEN}" -X POST --data '{ "body":"Hello from Travis" }' "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+   ocaml ./travis2github.ml
+
+   echo -n '{ "body" : "' > travis-comment.txt
+   echo -n "Hello from Travis job #${TRAVIS_BUILD_ID}" >> travis-comment.txt
+   echo '" }' >> travis-comment.txt
+   
+   curl --user "ocp-lint-bot:${GITHUB_TOKEN}" -X POST --data  @travis-comment.txt "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 
    cd ../..
    git clone https://github.com/OCamlPro/typerex-lint.git OCamlPro/typerex-lint
