b685a358818aeb002a55843ba075fd537bdd2e9e
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2016-05-25 15:11:12 +0200

    dsjdljsf

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -19,6 +19,13 @@ make install
 
 if [ "${OCAML_VERSION}" == "4.02.1" ] ; then
 
+  echo '${TRAVIS_PULL_REQUEST} = ' ${TRAVIS_PULL_REQUEST}
+  echo '${TRAVIS_REPO_SLUG} = ' ${TRAVIS_REPO_SLUG}
+  echo '${TRAVIS_TAG} = ' ${TRAVIS_TAG}
+  echo '${TRAVIS_BRANCH} = ' ${TRAVIS_BRANCH}
+  echo '${TRAVIS_COMMIT} = ' ${TRAVIS_COMMIT}
+  echo '${TRAVIS_COMMIT_RANGE} = ' ${TRAVIS_COMMIT_RANGE}
+    
    git clone https://github.com/OCamlPro/typerex-lint &&
    (cd typerex-lint;
     ./configure &&
