0f9b9dfa0a2bfb4b1c0a4b365733d42540c2fa90
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2016-05-25 15:08:50 +0200

    add ocp-lint in travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export OPAMYES=1 OPAMVERBOSE=1
 eval `opam config env`
@@ -14,6 +14,39 @@ opam --git-version
 PREFIX=$HOME/.opam/$OCAML_VERSION
 
 opam pin add my-package .
-opam install my-package
-opam remove my-package
 
+opam install -y my-package &> my-package.install_log
+
+curl -X POST --data  @my-package.install_log "http://github.lefessant.net:18080/travis\?issue=${TRAVIS_PULL_REQUEST}&token=${TRANSIT_TOKEN}"
+
+if [ $? -eq 0 ];then
+   echo "Installation OK"
+else
+   exit 2
+fi
+
+opam remove my-package &> my-package.remove_log
+
+curl -X POST --data  @my-package.remove_log "http://github.lefessant.net:18080/travis\?issue=${TRAVIS_PULL_REQUEST}&token=${TRANSIT_TOKEN}"
+if [ $? -eq 0 ];then
+   echo "Removal OK"
+else
+   exit 2
+fi
+
+if [ "${OCAML_VERSION}" != "4.02.3" ] ; then
+   echo No lint
+else
+
+  PROJECT=${TRAVIS_REPO_SLUG}
+
+   cd ../..
+   git clone https://github.com/OCamlPro/typerex-lint.git OCamlPro/typerex-lint
+   opam pin add -y typerex-lint OCamlPro/typerex-lint
+
+   ocp-lint --init
+   ocp-lint --path ${PROJECT} &> lint.log
+   head -n 50 lint.log
+   
+   curl -X POST --data  @lint.log "http://github.lefessant.net:18080/travis\?issue=${TRAVIS_PULL_REQUEST}&token=${TRANSIT_TOKEN}"
+fi
