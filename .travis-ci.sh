9542c7f5085e5716c854ee6a32da4a0794ec6762
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2016-06-13 16:35:31 +0200

    s;lsl;sl

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -16,11 +16,7 @@ PREFIX=$HOME/.opam/$OCAML_VERSION
 opam remove ocp-build
 opam pin remove -y typerex-lint
 
-opam pin add ocp-build .
-
-opam install -y --json=my-package.install_log ocp-build 
-
-head -n 10 my-package.install_log
+opam pin add ocp-build . 2> my-package.install_log
 
 curl -X POST --data  @my-package.install_log "http://github.lefessant.net:18080/travis?issue=${TRAVIS_PULL_REQUEST}&token=${TRANSIT_TOKEN}"
 
@@ -31,9 +27,9 @@ else
 fi
 
 opam remove my-package 2> my-package.remove_log
-echo opam install finished >> my-package.remove_log
 
-#curl -X POST --data  @my-package.remove_log "http://github.lefessant.net:18080/travis?issue=${TRAVIS_PULL_REQUEST}&token=${TRANSIT_TOKEN}"
+curl -X POST --data  @my-package.remove_log "http://github.lefessant.net:18080/travis?issue=${TRAVIS_PULL_REQUEST}&token=${TRANSIT_TOKEN}"
+
 if [ $? -eq 0 ];then
    echo "Removal OK"
 else
@@ -51,8 +47,8 @@ else
    opam pin add -y typerex-lint OCamlPro/typerex-lint
 
    ocp-lint --init
-   ocp-lint --path ${PROJECT} &> lint.log
-   head -n 50 lint.log
+   ocp-lint --path ${PROJECT} > lint.stdout 2> lint.stderr
+   cat lint.stdout lint.stderr > lint.log
    
-#   curl -X POST --data  @lint.log "http://github.lefessant.net:18080/travis?issue=${TRAVIS_PULL_REQUEST}&token=${TRANSIT_TOKEN}"
+   curl -X POST --data  @lint.log "http://github.lefessant.net:18080/travis?issue=${TRAVIS_PULL_REQUEST}&token=${TRANSIT_TOKEN}"
 fi
