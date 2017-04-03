3d438d0b7b05e5f2562dd81e9af7a54a20e5b0b2
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2017-04-03 23:08:21 +0200

    graph reduction and from-to in --graph-report

--- tools/ocp-build/engine/buildEngineReport.ml
+++ tools/ocp-build/engine/buildEngineReport.ml
@@ -131,148 +131,199 @@ type vertex = {
   vertex_id : int;
   vertex_name: string;
   vertex_package : BuildEngineTypes.build_package;
-  mutable vertex_all : vertex IntMap.t;
+  mutable vertex_to_all : vertex IntMap.t;
+  mutable vertex_from_all : vertex IntMap.t;
   mutable vertex_to : vertex IntMap.t;
   mutable vertex_from : vertex IntMap.t;
 }
 
 let report b =
   if !output_graph_report then
-  let rules_done = ref IntSet.empty in
-  let graph = ref IntMap.empty in
-  let vertices = Hashtbl.create 1111 in
-  let packages = ref IntMap.empty in
-  let nvertices = ref 0 in
-  let rec get_vertex file =
-    try
-      IntMap.find file.file_id !graph
-    with Not_found ->
-      let basename = file.file_basename in
-      let basename =
-
-        try
-          let pos = String.index basename '.' in
-          String.sub basename 0 pos
-        with
-        | _ ->  basename
-      in
-      let package_id = file.file_package.package_id in
-      let key = (package_id, basename) in
-      let v =
+    let rules_done = ref IntSet.empty in
+    let graph = ref IntMap.empty in
+    let vertices = Hashtbl.create 1111 in
+    let packages = ref IntMap.empty in
+    let nvertices = ref 0 in
+    let rec get_vertex file =
       try
-        Hashtbl.find vertices key
+        IntMap.find file.file_id !graph
       with Not_found ->
+        let basename = file.file_basename in
+        let basename =
 
-        let v = {
-          vertex_id = !nvertices;
-          vertex_name = basename;
-          vertex_package = file.file_package;
-          vertex_all = IntMap.empty;
-          vertex_to = IntMap.empty;
-          vertex_from = IntMap.empty;
-        } in
-        incr nvertices;
-        Hashtbl.add vertices key v;
-
-        let (p,vertices_in_package) =
           try
-            IntMap.find file.file_package.package_id !packages
-          with Not_found ->
-            let r = ref [] in
-            packages := IntMap.add file.file_package.package_id
-              (file.file_package,r) !packages;
-            (file.file_package, r)
+            let pos = String.index basename '.' in
+            String.sub basename 0 pos
+          with
+          | _ ->  basename
         in
-        vertices_in_package := v :: !vertices_in_package;
+        let package_id = file.file_package.package_id in
+        let key = (package_id, basename) in
+        let v =
+          try
+            Hashtbl.find vertices key
+          with Not_found ->
 
+            let v = {
+              vertex_id = !nvertices;
+              vertex_name = basename;
+              vertex_package = file.file_package;
+              vertex_to_all = IntMap.empty;
+              vertex_from_all = IntMap.empty;
+              vertex_to = IntMap.empty;
+              vertex_from = IntMap.empty;
+            } in
+            incr nvertices;
+            Hashtbl.add vertices key v;
+
+            let (p,vertices_in_package) =
+              try
+                IntMap.find file.file_package.package_id !packages
+              with Not_found ->
+                let r = ref [] in
+                packages := IntMap.add file.file_package.package_id
+                  (file.file_package,r) !packages;
+                (file.file_package, r)
+            in
+            vertices_in_package := v :: !vertices_in_package;
+
+            v
+        in
+        graph := IntMap.add file.file_id v !graph;
         v
-      in
-      graph := IntMap.add file.file_id v !graph;
-      v
 
-  and add_rule rule =
-    if not (IntSet.mem rule.rule_id !rules_done) then begin
-      rules_done := IntSet.add rule.rule_id !rules_done;
+    and add_rule rule =
+      if not (IntSet.mem rule.rule_id !rules_done) then begin
+        rules_done := IntSet.add rule.rule_id !rules_done;
 
-      IntMap.iter (fun target_id target_file ->
+        IntMap.iter (fun target_id target_file ->
 
-        let target_vertex = get_vertex target_file in
-        IntMap.iter (fun source_id source_file ->
-          let source_vertex = get_vertex source_file in
+          let target_vertex = get_vertex target_file in
+          IntMap.iter (fun source_id source_file ->
+            let source_vertex = get_vertex source_file in
 
-          if source_vertex != target_vertex then begin
-            target_vertex.vertex_from <- IntMap.add
-              source_vertex.vertex_id source_vertex target_vertex.vertex_from;
+            if source_vertex != target_vertex then begin
+              target_vertex.vertex_from <- IntMap.add
+                source_vertex.vertex_id source_vertex target_vertex.vertex_from;
 
-            source_vertex.vertex_to <- IntMap.add
-              target_vertex.vertex_id target_vertex source_vertex.vertex_to;
-          end
-        ) rule.rule_sources
-      ) rule.rule_targets
-    end
-  in
-
-  Hashtbl.iter (fun _file_id file ->
-    let (_ : vertex) = get_vertex file in ()
-  ) b.build_files;
-  Hashtbl.iter (fun _rule_id rule -> add_rule rule) b.build_rules;
-
-
-  let rec iter v =
-    if IntMap.is_empty v.vertex_all then begin
-      v.vertex_all <- v.vertex_from;
-      IntMap.iter (fun _ v2 ->
-        iter v2;
-        IntMap.iter (fun _ v3 ->
-          v.vertex_all <- IntMap.add v3.vertex_id v3 v.vertex_all
-        ) v2.vertex_all
-      ) v.vertex_from;
-      IntMap.iter (fun _ v2 ->
-        IntMap.iter (fun _ v3 ->
-          if IntMap.mem v2.vertex_id v3.vertex_all then
-            v.vertex_from <- IntMap.remove v2.vertex_id v.vertex_from
-        ) v.vertex_from
-      ) v.vertex_from
-    end
-  in
-  Hashtbl.iter (fun _ v -> iter v) vertices;
-
-  BuildMisc.safe_mkdir report_dir;
-  IntMap.iter (fun package_id (p, vertices_in_package) ->
-
-    let t = Ocamldot.create p.package_name [] in
-    let nodes = ref IntMap.empty in
-    let attrs = ref [NodeColor "blue"; NodeShape Box] in
-    let get_node v =
-      try
-        IntMap.find v.vertex_id !nodes
-      with Not_found ->
-        let node = Ocamldot.node t
-          (Printf.sprintf "%s:%s"
-             v.vertex_package.package_name
-             v.vertex_name) !attrs in
-        nodes := IntMap.add v.vertex_id node !nodes;
-        node
+              source_vertex.vertex_to <- IntMap.add
+                target_vertex.vertex_id target_vertex source_vertex.vertex_to;
+            end
+          ) rule.rule_sources
+        ) rule.rule_targets
+      end
     in
 
-    List.iter (fun v ->
-      let node = get_node v in
-      ()
-    ) !vertices_in_package;
-    attrs := [];
-    List.iter (fun v ->
-      let node = get_node v in
-      IntMap.iter (fun _ v2 ->
-        let node2 = get_node v2 in
-        Ocamldot.add_edge node2 node [];
-      ) v.vertex_from
-    ) !vertices_in_package;
+    Hashtbl.iter (fun _file_id file ->
+      let (_ : vertex) = get_vertex file in ()
+    ) b.build_files;
+    Hashtbl.iter (fun _rule_id rule -> add_rule rule) b.build_rules;
 
 
-    Ocamldot.save t
-      (Printf.sprintf "_obuild/_reports/package_%s.dot" p.package_name)
+    let rec iter_from v =
+      if IntMap.is_empty v.vertex_from_all then begin
+        v.vertex_from_all <- v.vertex_from;
+        IntMap.iter (fun _ v2 ->
+          iter_from v2;
+          IntMap.iter (fun _ v3 ->
+            v.vertex_from_all <- IntMap.add v3.vertex_id v3 v.vertex_from_all
+          ) v2.vertex_from_all
+        ) v.vertex_from;
+        IntMap.iter (fun _ v2 ->
+          if v2.vertex_package == v.vertex_package then
+            IntMap.iter (fun _ v3 ->
+              if v3.vertex_package == v.vertex_package &&
+                IntMap.mem v2.vertex_id v3.vertex_from_all then
+                v.vertex_from <- IntMap.remove v2.vertex_id v.vertex_from
+          ) v.vertex_from
+        ) v.vertex_from
+      end
+    in
 
-  ) !packages;
+
+    Hashtbl.iter (fun _ v ->
+      iter_from v;
+    ) vertices;
+
+    let rec iter_to v =
+      if IntMap.is_empty v.vertex_to_all then begin
+        v.vertex_to_all <- v.vertex_to;
+        IntMap.iter (fun _ v2 ->
+          iter_to v2;
+          IntMap.iter (fun _ v3 ->
+            v.vertex_to_all <- IntMap.add v3.vertex_id v3 v.vertex_to_all
+          ) v2.vertex_to_all
+        ) v.vertex_to;
+        IntMap.iter (fun _ v2 ->
+          if v2.vertex_package == v.vertex_package then
+            IntMap.iter (fun _ v3 ->
+              if v3.vertex_package == v.vertex_package &&
+                IntMap.mem v2.vertex_id v3.vertex_to_all then
+              v.vertex_to <- IntMap.remove v2.vertex_id v.vertex_to
+          ) v.vertex_to
+        ) v.vertex_to
+      end
+    in
 
 
-  ()
+    Hashtbl.iter (fun _ v ->
+      iter_to v) vertices;
+
+    BuildMisc.safe_mkdir report_dir;
+
+    IntMap.iter (fun package_id (p, vertices_in_package) ->
+
+      let create name get_edges =
+
+        let t = Ocamldot.create p.package_name
+          [ GraphSize (11.7, 8.3); Ratio RatioFill ]
+        in
+        let nodes = ref IntMap.empty in
+
+        let get_node v =
+          try
+            IntMap.find v.vertex_id !nodes
+          with Not_found ->
+            let node = Ocamldot.node t v.vertex_name
+              [NodeColor "blue"; NodeShape Box]
+            in
+            nodes := IntMap.add v.vertex_id node !nodes;
+            node
+        in
+
+        List.iter (fun v ->
+          let node = get_node v in
+          ()
+        ) !vertices_in_package;
+
+        let get_node v =
+          try
+            IntMap.find v.vertex_id !nodes
+          with Not_found ->
+            let node = Ocamldot.node t
+              (Printf.sprintf "%s:%s"
+                 v.vertex_package.package_name
+                 v.vertex_name) [] in
+            nodes := IntMap.add v.vertex_id node !nodes;
+            node
+        in
+
+        List.iter (fun v ->
+          let node = get_node v in
+          IntMap.iter (fun _ v2 ->
+            let node2 = get_node v2 in
+            Ocamldot.add_edge node2 node [];
+          ) (get_edges v)
+        ) !vertices_in_package;
+
+
+        Ocamldot.save t
+          (Printf.sprintf "_obuild/_reports/package_%s_%s.dot"
+             name p.package_name)
+      in
+      create "from" (fun v -> v.vertex_from);
+      create "to" (fun v -> v.vertex_to);
+    ) !packages;
+
+
+    ()
